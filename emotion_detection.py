102fb027e7efda3a184e2ea72c6d0020f66884d1
Maithri Hegde <maithri24@gmail.com>
2026-07-02 10:08:10 -0400

    formatting changes

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,17 @@
 import requests
+import json as js
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json=myobj, headers=headers)
-    return response.text
+    formatted_response=js.loads(response.text)
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotions_dict = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+    return {'anger': anger_score,'disgust': disgust_score,'fear': fear_score,'joy': joy_score,'sadness': sadness_score,'dominant_emotion': dominant_emotion}
