229eb06eb74d4595d1a503557460498a2c63395d
Maithri Hegde <maithri24@gmail.com>
2026-07-03 08:42:07 -0400

    resolved lint error

--- server.py
+++ server.py
@@ -55,7 +55,12 @@ def emotions_detector():
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-    return f"For the given statement, the system response is \'anger\': {response['anger']}, \'disgust\': {response['disgust']}, \'fear\': {response['fear']}, \'joy\': {response['joy']} and \'sadness\': {response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    return (
+        f"For the given statement, the system response is \'anger\':"
+        f"{response['anger']}, \'disgust\': {response['disgust']}, \'fear\':"
+        f"{response['fear']}, \'joy\': {response['joy']} and \'sadness\':"
+        f"{response['sadness']}. The dominant emotion is {response['dominant_emotion']}."
+    )
 
 @app.route('/')
 def render_index():
