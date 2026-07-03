77dab75dfa78123d194a2ff62c5bf4b5c60a05db
Maithri Hegde <maithri24@gmail.com>
2026-07-03 07:10:04 -0400

    Added web development scaffolding

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import request, Flask, render_template
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask('Emotion detector')
+
+@app.route('/emotionDetector')
+def emotionDetector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    return response
+
+@app.route('/')
+def render_index():
+    return render_template('index.html')
+
+if __name__ == 'main':
+    app.run(host='0.0.0.0',port=5000)
